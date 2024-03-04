aca9c041cc66764462a3fba5ff756a818e0f4c69
Shivang Yadav <shivangyadav39@gmail.com>
2024-03-04 16:55:27 +0530

    added component

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+<Inject services={[Day, Week, WorkWeek, Month, Agenda ]} />
+
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import * as React from 'react';
+import './App.css';
+function App() {
+  const data = [
+    {
+      Id: 1,
+      Subject: 'Meeting',
+      StartTime: new Date(2023, 1, 15, 10, 0),
+      EndTime: new Date(2023, 1, 15, 12, 30),
+    },
+  ];
+  return (
+    <ScheduleComponent
+      selectedDate={new Date(2023, 1, 15)}
+      eventSettings={{
+        dataSource: data,
+      }}
+    >
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
+}
+export default App;
